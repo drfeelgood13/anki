83f4fbe4e10cb24f807001198c052f0f10a3792a
Lukas Jokubauskas <jokubauskas.lukas@gmail.com>
2024-06-27 15:55:52 +0200

    Method 1

--- pylib/anki/_vendor/stringcase.py
+++ pylib/anki/_vendor/stringcase.py
@@ -110,22 +110,6 @@ def pascalcase(string):
     return capitalcase(camelcase(string))
 
 
-def pathcase(string):
-    """Convert string into path case.
-    Join punctuation with slash.
-
-    Args:
-        string: String to convert.
-
-    Returns:
-        string: Path cased string.
-
-    """
-    string = snakecase(string)
-    if not string:
-        return string
-    return re.sub(r"_", "/", string)
-
 
 def backslashcase(string):
     """Convert string into spinal case.
